92f0777784380d1c83524b04eed59bbea261898b
Hurricane0 <nikitadv777@gmail.com>
2020-01-11 19:15:47 +0200

    Updates

---
+++ precache-manifest.244a6047023dbe972d21fe051b78eef8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98821c59fd950bef79afcdc1308d5198",
+    "url": "/Todo-List/index.html"
+  },
+  {
+    "revision": "b439e6fd995ac1149c93",
+    "url": "/Todo-List/static/css/main.a1daa979.chunk.css"
+  },
+  {
+    "revision": "86672e3f2cf9b9ffcd9a",
+    "url": "/Todo-List/static/js/2.1fbcb4c6.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Todo-List/static/js/2.1fbcb4c6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b439e6fd995ac1149c93",
+    "url": "/Todo-List/static/js/main.79ed047a.chunk.js"
+  },
+  {
+    "revision": "6c43551b69127942d60e",
+    "url": "/Todo-List/static/js/runtime-main.e5fd31be.js"
+  }
+]);
